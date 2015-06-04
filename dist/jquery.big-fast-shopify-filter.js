a37b4941d7f6289ea6b43694a60c55522ea22146
Colin Brogan <colinbrogan@gmail.com>
2015-06-04 10:56:15 -0500

    Private instance methods and custom events added, ready to rock and roll

--- dist/jquery.big-fast-shopify-filter.js
+++ dist/jquery.big-fast-shopify-filter.js
@@ -61,6 +61,26 @@
 				this.init();
 		}
 
+		/******* Private Member Variables go here *****/
+		var privateInfo = null;
+
+		/******* Private Methods go here ******/
+
+		var somePrivateMethod = function(info) {
+			privateInfo = info;
+			console.log("somePrivateMethod fired with "+privateInfo);
+		};
+
+		var renderTemplate = function(data) {
+			/**** process data here ****/
+
+			return [
+			"<div>",
+			data.title,
+			"</div>"
+			].join();
+		};
+
 		// Avoid Plugin.prototype conflicts
 		$.extend(Plugin.prototype, {
 				init: function () {
@@ -73,21 +93,114 @@
 						console.log("xD");
 						console.log("Defaults Are:");
 						console.log(defaults);
-						console.log("changed againh");
+						console.log(this.element);
 				},
-				yourOtherFunction: function () {
-						// some logic
+				/********** instance variables  ****************/
+				filtered: null,
+				allRetrieved: null,
+				/********** public Methods ***************/
+				filter: function(params) {
+					for(var param in params) {
+						console.log(param+":"+params[param]);
+					}
+					$(this.element).trigger("loadReceived");   
+				},
+				trickleToGrid: function(load) {
+					for(var something in load) {
+						renderTemplate(load[something]);
+					}
+					console.log(load);
+					console.log("hello");
+					somePrivateMethod(load);
+					console.log("trickleToGrid set someInfo2 through private method to "+privateInfo);
+				},
+				refresh: function() {
+					console.log("refresh");
+					somePrivateMethod("refresh");
+				},
+
+				/********* Public Setters ****************/
+				setSomeInfo: function(info) {
+					this.someInfo = info;
+					console.log("Set someInfo to "+this.someInfo);
+				},
+
+
+				/********* Public Getters ****************/
+				getSomeInfo: function() {
+					return this.someInfo;
+				},
+
+				/********** Public Getters of Private Info ***************/
+				getPrivateInfo: function() {
+					return privateInfo;
 				}
+
 		});
 
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
 		$.fn[ pluginName ] = function ( options ) {
-				return this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
+				if(this.data("fast-start") !== undefined) {
+
+				}
+				/****** custom events go here ******/
+
+				this.on("loadReceived",function() {
+					console.log("loadReceived Fired");
 				});
+
+				var args = arguments;
+
+		        // Is the first parameter an object (options), or was omitted,
+		        // instantiate a new instance of the plugin.
+		        if (options === undefined || typeof options === "object") {
+		            return this.each(function () {
+
+		                // Only allow the plugin to be instantiated once,
+		                // so we check that the element has no plugin instantiation yet
+		                if (!$.data(this, "plugin_" + pluginName)) {
+
+		                    // if it has no instance, create a new one,
+		                    // pass options to our plugin constructor,
+		                    // and store the plugin instance
+		                    // in the elements jQuery data object.
+		                    $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+		                }
+		            });
+
+		        // If the first parameter is a string and it doesn"t start
+		        // with an underscore or "contains" the `init`-function,
+		        // treat this as a call to a public method.
+		        } else if (typeof options === "string" && options[0] !== "_" && options !== "init") {
+
+		            // Cache the method call
+		            // to make it possible
+		            // to return a value
+		            var returns;
+
+		            this.each(function () {
+		                var instance = $.data(this, "plugin_" + pluginName);
+
+		                // Tests that there"s already a plugin-instance
+		                // and checks that the requested public method exists
+		                if (instance instanceof Plugin && typeof instance[options] === "function") {
+
+		                    // Call the method of our plugin instance,
+		                    // and pass it the supplied arguments.
+		                    returns = instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+		                }
+
+		                if (options === "destroy") {
+		                  $.data(this, "plugin_" + pluginName, null);
+		                }
+		            });
+
+		            // If the earlier cached method
+		            // gives a value back return the value,
+		            // otherwise return this to preserve chainability.
+		            return returns !== undefined ? returns : this;
+		        }
 		};
 
 })( jQuery, window, document );
