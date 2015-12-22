98945d813851898cb16bd40b3336e19d8aceae71
Colin Brogan <colinbrogan@gmail.com>
2015-12-21 18:13:20 -0600

    Make this damn thing public

--- src/Shopify.pipeInCollection.js
+++ src/Shopify.pipeInCollection.js
@@ -2,52 +2,111 @@
 Shopify.Mazer = Shopify.Mazer || {};
 Shopify.Mazer.pipeInCollection = {
 	keepGoing: true,
-	doneTrack: {},
+	doneTrack: [],
+	loadIn: [],
+	loadBatch: 0,
 	go: function(collection,doWithEachLoad,pageStart,whenDone) {
+					console.log("Starting new pipeInCollection with promises");
 					var me = Shopify.Mazer.pipeInCollection;
+					me.loadIn = [];
 					if(pageStart == undefined) {
 						pageStart = 1;
 					}
+					var promises = [];
 					for(var i = pageStart; i <= pageStart+6; i++) {
 					  		var page = "";
 					  		if(i > 1) {
 					  			page = "&page="+i;
 					  		}
-/*
-					  		console.log('Shopify.Mazer.pipeInAjax: hit page '+page);
-*/
 							var callback = function(response) {
-							  	var response = $.parseJSON(response);
 							  	Shopify.Mazer.pipeInCollection.doneTrack[response.page] = true;
 							  	if(Shopify.Mazer.utilities.keyCount(response.products) == 0) {
 							  		me.keepGoing = false;
-							  		console.log("Shopify.Mazer.pipeInCollection: Ended '"+response.handle+"' at page "+response.page);
 							  	} else {
 							  		doWithEachLoad(response);
 							  	}
 							  	var allFiredFinished = true;
-							  	for(pageLog in Shopify.Mazer.pipeInCollection.doneTrack) {
-							  		if(Shopify.Mazer.pipeInCollection.doneTrack[pageLog] == false) {
-							  			allFiredFinished = false;
-							  		}
-							  	}
-							  	if(allFiredFinished && me.keepGoing == false) {
-							  		console.log("whenDone should attempt to fire if not undefined");
-							  		if(whenDone !== undefined) {
-							  			console.log("whenDone fired");
-							  			console.log(whenDone);
-							  			whenDone(response);
-							  		}
-							  	}
-							  	if(Number(response.page) % 6 == 0 && me.keepGoing == true) {
-							  		me.go(collection,doWithEachLoad,Number(response.page)+1);
-							  	}
 					  		};
 							Shopify.Mazer.pipeInCollection.doneTrack[i] = false;
-					  		$.get('/collections/'+collection+'?view=ajaxy'+page,function(response) {
-					  			callback(response);
-					  		});
+							// fire 7 simaltaneous ajax requests
+							promises.push(
+						  		$.ajax('/collections/'+collection+'?view=ajaxy'+page)
+						  	);
+						  	var arrLength = me.loadIn.push(
+						  		jQuery.Deferred()
+						  	);
+
+						  	promises[i - pageStart].done(function(response) {
+						  			var response = $.parseJSON(response);
+						  			// make sure 6 returned requests fire callback in sequential page order, rather than "when-they-come-in"
+						  			switch((response.page - 1) % 7) {
+						  				case 0:
+						  					callback(response);
+						  					console.log("Load in on page "+response.page)
+						  					me.loadIn[0].resolve();
+						  					break;
+						  				case 1:
+						  					$.when(me.loadIn[0]).done(function(data) {
+						  						callback(response);
+						  						console.log("Load in on page "+response.page)
+						  						me.loadIn[1].resolve();
+						  					});
+						  					break;
+						  				case 2:
+						  					$.when(me.loadIn[0],me.loadIn[1]).done(function(data) {
+						  						callback(response);
+						  						console.log("Load in on page "+response.page)
+						  						me.loadIn[2].resolve();
+						  					});
+						  					break;
+						  				case 3:
+						  					$.when(me.loadIn[0],me.loadIn[1],me.loadIn[2]).done(function(data) {
+						  						callback(response);
+						  						console.log("Load in on page "+response.page)
+						  						me.loadIn[3].resolve();
+						  					});
+						  					break;
+						  				case 4:
+						  					$.when(me.loadIn[0],me.loadIn[1],me.loadIn[2],me.loadIn[3]).done(function(data) {
+						  						callback(response);
+						  						console.log("Load in on page "+response.page)
+						  						me.loadIn[4].resolve();
+						  					});
+						  					break;
+						  				case 5:
+						  					$.when(me.loadIn[0],me.loadIn[1],me.loadIn[2],me.loadIn[3],me.loadIn[4]).done(function(data) {
+						  						callback(response);
+						  						console.log("Load in on page "+response.page)
+						  						me.loadIn[5].resolve();
+						  					});
+						  					break;
+						  				case 6:
+						  					$.when(me.loadIn[0],me.loadIn[1],me.loadIn[2],me.loadIn[3],me.loadIn[4],me.loadIn[5]).done(function(data) {
+						  						callback(response);
+						  						console.log("Load in on page "+response.page)
+						  						me.loadIn[6].resolve();
+						  					});
+						  					break;
+						  			}
+
+						  	});
 				  	}
+				  	$.when(me.loadIn[0],me.loadIn[1],me.loadIn[2],me.loadIn[3],me.loadIn[4],me.loadIn[5],me.loadIn[6]).done(function( data, textStatus, jqXHR ) {
+			
+
+						me.loadBatch = me.loadBatch + 1;
+						if(me.keepGoing == false) {
+					  		if(whenDone !== undefined) {
+					  			console.log("All loads are in");
+					  			whenDone(data);
+					  		}
+						} else {
+							me.loadIn = [];
+							me.go(collection,doWithEachLoad,(7*me.loadBatch+1),whenDone);
+						}
+					});
+
+
 	},
 	/* private methods for class */
 }
